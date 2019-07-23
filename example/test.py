cf3cda2825120e0abbf3e40772ee7865100f36d7
Alexandru Rusu <aqrusu@gmail.com>
2019-07-23 15:00:25 +0300

    Fixed includes

--- example/test.py
+++ example/test.py
@@ -1,1 +1,9 @@
-from lmso-algorithm import lmso_algorithm
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Alexandru - George Rusu (2019). All Rights Reserved.
+"""
+from lmso_algorithm import algorithm
+
+a = algorithm.lmso()
+b = algorithm.lmso()
