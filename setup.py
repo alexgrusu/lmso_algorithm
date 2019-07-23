d5a932405b6026d9a15aef0edf50a7e0a8fa0639
Alexandru Rusu <aqrusu@gmail.com>
2019-07-23 15:23:57 +0300

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from pathlib import Path
 
 setuptools.setup(
   name = "lmso_algorithm",
-  version = 1.0,
+  version = 1.1,
   long_description = Path("README.md").read_text(),
-  packages = setuptools.find_packages(exclude = ["tests", "example"])
-
+  packages = setuptools.find_packages(exclude = ["tests", "example"]),
+  author="Alexandru - George Rusu",
+  author_email="aqrusu@gmail.com",
+  description="An Optimized LMS Algorithm",
+  keywords="Adaptive filters, Echo cancellation, System identification",
+  url="https://github.com/alexgrusu/lmso_algorithm",   # project home page, if any
 )
